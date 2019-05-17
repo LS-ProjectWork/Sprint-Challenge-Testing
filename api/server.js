a634663d8669bc917f5c259f6a038cab280ed60b
Darien Payton <darien.payton71@gmail.com>
2019-05-17 12:33:36 -0400

    updated post operation logic

--- api/server.js
+++ api/server.js
@@ -9,15 +9,14 @@ const server = express()
 server.use(express.json());
 
 server.post('/games', async (req, res) => {
-    if(req.body) {
-        await db('games')
-            .insert(req.body)
-            .then(state => {
-                res.status(200).json(state)
-            })
-    } else {
-        return res.status(422)
-    }
+    await db('games')
+        .insert(req.body)
+        .then(state => {
+            res.status(200).json(state)
+        })
+        .catch(err => {
+            res.status(422).send(err)
+        })
 })
 
 server.get('/games', async (req, res) => {
